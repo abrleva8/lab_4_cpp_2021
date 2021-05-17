2ae67d719fab82c2a6eaf5de6e55a60d4c9a6802
Левон Абрамян <abrleva8@gmail.com>
2021-05-17 23:36:30 +0300

    Revert "add sentences"
    This reverts commit 4c48c1d52c2003af548628df7ef3747c7726b2cd.

--- text_analyzer.cpp
+++ text_analyzer.cpp
@@ -1,7 +1,6 @@
 #include "text_analyzer.h"
 
 #include <iostream>
-#include <sstream>
 
 Text::Text(std::vector<std::string> data) {
 	this->data = data;
@@ -10,8 +9,6 @@ Text::Text(std::vector<std::string> data) {
 	this->count_number_of_words();
 	this->count_number_of_lines();
 	this->count_number_of_paragraphs();
-	this->set_sentences();
-	this->set_data_sentences();
 }
 
 Text::Text() {
@@ -21,20 +18,12 @@ void Text::count_number_of_lines() {
 	this->number_of_lines = data.size();
 }
 
-bool Text::is_end_symbol(char c) {
-	return c == '.' || c == '!' || c == '?';
-}
-
 bool Text::is_visible(char c) {
 	return '!' <= c && c <= '~';
 }
 
 bool Text::is_letter(char c) {
-	return 'a' <= c && c <= 'z' || is_capital_letter(c);
-}
-
-bool Text::is_capital_letter(char c) {
-	return 'A' <= c && c <= 'Z';
+	return 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z';
 }
 
 void Text::count_symbols() {
@@ -57,41 +46,30 @@ void Text::count_visible_symbols() {
 	this->number_of_visible_symbols = result;
 }
 
-int Text::count_number_of_words(std::string str) {
-	int result = 0;
-	bool is_new_word = false;
-	for (int i = 0; i < str.size(); i++) {
-		if (is_letter(str[i])) {
-			if (i == str.size() - 1) {
-				result++;
-			}
-			is_new_word = true;
-		} else {
-			if (is_new_word) {
-				result++;
-				is_new_word = false;
-			}
-		}
-	}
-
-	return result;
-}
-
 void Text::count_number_of_words() {
 	int result = 0;
 	for (auto str : this->data) {
-		result += count_number_of_words(str);
+		bool is_new_word = false;
+		for (int i = 0; i < str.size(); i++) {
+			if (is_letter(str[i])) {
+				if (i == str.size() - 1) {
+					result++;
+				}
+				is_new_word = true;
+			} else {
+				if (is_new_word) {
+					result++;
+					is_new_word = false;
+				}
+			}
+		}
 	}
 	this->number_of_words = result;
 }
 
 void Text::print(std::ostream* stream) {
-	size_t size = data.size();
-	for (size_t i = 0; i < data.size(); i++) {
-		*stream << data[i];
-		if (i != size - 1) {
-			*stream << std::endl;
-		}
+	for (auto str : data) {
+		*stream << str << std::endl;
 	}
 }
 
@@ -100,21 +78,7 @@ void Text::print_info(std::ostream* stream) {
 	*stream << "The numbers of visible symbols in the text equals " << this->number_of_visible_symbols << std::endl;
 	*stream << "The numbers of words in the text equals " << this->number_of_words << std::endl;
 	*stream << "The numbers of lines in the text equals " << this->number_of_lines << std::endl;
-	*stream << "The numbers of paragraphs in the text equals " << this->number_of_paragraphs;
-	if (stream->rdbuf() == std::cout.rdbuf()) {
-		*stream << std::endl;
-	}
-}
-
-void Text::print_sentences(std::ostream* stream) {
-	for (auto str : this->sentences) {
-		std::cout << str << std::endl;
-	}
-}
-
-void Text::print_sentences_info(std::ostream* stream) {
-	for (std::map<int, int>::iterator it = sentence_data.begin(); it != sentence_data.end(); ++it)
-		std::cout << it->first << " => " << it->second << std::endl;
+	*stream << "The numbers of paragraphs in the text equals " << this->number_of_paragraphs << std::endl;
 }
 
 void Text::count_number_of_paragraphs() {
@@ -134,42 +98,4 @@ void Text::count_number_of_paragraphs() {
 		}
 	}
 	this->number_of_paragraphs = result;
-}
-
-void Text::set_sentences() {
-	
-	std::stringstream ss;
-	for (auto str : this->data) {
-		int start_sentence = -1;
-		int end_sentence = -1;
-		for (int i = 0; i < str.length(); i++) {
-			if (is_capital_letter(str[i]) && end_sentence == -1) {
-				start_sentence = i;
-				end_sentence = i;
-			}
-
-			if (end_sentence != -1) {
-				end_sentence++;
-				ss << str[i];
-			}
-
-			if (is_end_symbol(str[i]) && start_sentence != -1) {
-				this->sentences.push_back(ss.str());
-				ss.str("");
-				start_sentence = -1;
-				end_sentence = -1;
-			}
-		}
-	}
-}
-
-void Text::set_data_sentences() {
-	for (auto str : sentences) {
-		int number = count_number_of_words(str);
-		if (sentence_data.find(number) == sentence_data.end()) {
-			sentence_data.insert(std::pair<int, int>(number, 1));
-		} else {
-			sentence_data[number]++;
-		}
-	}
 }
--- text_analyzer.h
+++ text_analyzer.h
@@ -2,15 +2,12 @@
 #define _TEXT_ANALYZER_H_
 
 #include <vector>
-#include <map>
 #include <string>
 #include <iostream>
 
 class Text {
 private:
 	std::vector<std::string> data;
-	std::vector<std::string> sentences;
-	std::map<int, int> sentence_data;
 	int number_of_symbols = 0;
 	int number_of_visible_symbols = 0;
 	int number_of_words = 0;
@@ -19,26 +16,18 @@ private:
 
 	void count_symbols();
 	void count_visible_symbols();
-	int count_number_of_words(std::string str);
 	void count_number_of_words();
 	void count_number_of_lines();
 	void count_number_of_paragraphs();
 
-	void set_sentences();
-	void set_data_sentences();
-
-	bool is_end_symbol(char c);
 	bool is_visible(char c);
 	bool is_letter(char c);
-	bool is_capital_letter(char c);
 
 public:
 	Text(std::vector<std::string> data);
 	Text();
 	void print(std::ostream* stream = &std::cout);
 	void print_info(std::ostream* stream = &std::cout);
-	void print_sentences(std::ostream* stream = &std::cout);
-	void print_sentences_info(std::ostream* stream = &std::cout);
 };
 
 #endif
